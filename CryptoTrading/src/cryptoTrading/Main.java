b2451b263bed65f9782d9c59eedae02a6c280879
Martin Becker <martinbecker21@googlemail.com>
2024-11-26 15:02:34 +0100

    update

--- CryptoTrading/src/cryptoTrading/Main.java
+++ CryptoTrading/src/cryptoTrading/Main.java
@@ -1,16 +1,8 @@
 package cryptoTrading;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
 		Trade trade = new Trade();
 		//Trade Attribute:
 		trade.setCoinName(Tools.getStringFromUser("Welchen Coin hast du gekauft?"));
@@ -38,8 +30,6 @@ public class Main {
 		
 		System.out.println("Dein return on investment war bei " + trade.getROE() + "%");
 		System.out.println("----------------------");
-		
-		String filePath = "C:\\Users\\marti\\git\\MartinRepo\\CryptoTrading\\src\\cryptoTrading\\trades.json";
 		
 	}
 }
---
+++ CryptoTrading/src/cryptoTrading/WriteDataToJson.java
@@ -0,0 +1,11 @@
+package cryptoTrading;
+
+
+public class WriteDataToJson {
+
+	public static void main(String[] args){
+		
+
+	}
+
+}
